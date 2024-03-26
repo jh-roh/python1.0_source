5a94f1797ed15ef8142fe672ff4d1f69edf8191d
njh8810 <njh8810@naver.com>
2024-03-26 09:46:20 +0900

    파이썬 관련 예제 테스트 소스 커밋

---
+++ python_test/chapter02_01.py
@@ -0,0 +1,26 @@
+#Chapter02-1
+#파이썬 완전 기초
+#Print 사용법
+
+
+#기본출력
+print('Python Start!')
+print("Python Start!")
+print('''Python Start!''')
+
+print()
+print("""Python Start!""")
+
+
+#seperator 옵션
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('010', '7777','7777', sep='-')
+print('python', 'google', sep='@')
+
+print()
+
+#end 옵션
+
+
+
+
